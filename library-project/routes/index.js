5f60fed5a5d3299ba9a69393d48ba81ab9404d30
Sebastian Montoya <seb246@gmail.com>
2021-01-07 21:04:00 -0500

    read documents

--- library-project/routes/index.js
+++ library-project/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
 
+const Book = require(`../models/Book.model.js`);
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get(`/books`, (req,res,next) => {
+  Book.find()
+    .then(allBooks => {
+      res.render(`book-list`, {books: allBooks});
+    })
+    .catch(err => {
+      console.log(`error while getting books due to ${err}`);
+    })
 });
 
 module.exports = router;
